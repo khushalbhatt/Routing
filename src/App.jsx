4d5dfdb77bee00da5afca22d8482b3bc403ec699
Khushal Kumar <khushalbhatt17@gmail.com>
2025-02-22 13:40:03 +0530

    Lazy load part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState,lazy } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './Components/Landing'
-import Dashboard from './Components/Dashboard'
-import Header from './Components/Header'
+const Header = lazy(()=>import('./Components/Header'));
+const Landing = lazy(()=>import('./Components/Landing'));
+const Dashboard = lazy(()=>import('./Components/Dashboard'));
+
 
 function App() {
   const [count, setCount] = useState(0)
